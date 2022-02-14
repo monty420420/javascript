4b62dfe06dd34c19a911bf5d80e787f1eda1a48c
Muntaeyeon <monty420420@naver.com>
2022-02-14 23:47:56 +0900

    promise3

--- 12_callbackHell_to_promise.js
+++ 12_callbackHell_to_promise.js
@@ -1,25 +1,30 @@
 //Callback Hell example
 class UserStorage {
-    logUser(id, password, onSuccess, onError) {
+    logUser(id, password) {
+
+        return new Promise((resolve, reject) => {
         setTimeout(()=>{
             if(
                 (id === 'ten' && password === 'dream') ||
                 (id === 'coder' && password === 'coding')
             ){
-                onSuccess(id);
+                resolve(id);
             } else {
-                onError(new Error('not found'));
+                reject(new Error('not found'));
             }
         }, 2000);
-    }
+    });
+}
 
-    getRoles(user, onSuccess, onError){
-       setTimeout(() => {
-          if(user === 'ten') {
-              onSuccess({name: 'ten', role: 'admin'});
-          } else {
-              onError(new Error('no access'));
-          }
-       }, 1000);
+    getRoles(user){
+       return new Promise((resolve, reject) =>{
+        setTimeout(() => {
+            if(user === 'ten') {
+                resolve({name: 'ten', role: 'admin'});
+            } else {
+                reject(new Error('no access'));
+            }
+         }, 1000);
+       });
     }
 }
