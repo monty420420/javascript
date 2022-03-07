cdf8e34990cb0094a0b63584a71424a9981f9711
Muntaeyeon <monty420420@naver.com>
2022-03-07 23:27:23 +0900

    variable

--- 03_variable.js
+++ 03_variable.js
@@ -83,3 +83,5 @@ text = '7' + 5;
 console.log(`value: ${text}, type: ${typeof text}`);
 text = '8' / '2';
 console.log(`value: ${text}, type: ${typeof text}`);
+text = '3' / '1';
+console.log(`value: ${text}, type: ${typeof text}`);
