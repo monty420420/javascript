9a708639598fa45c775879114bdfcaf2087b6a7a
Muntaeyeon <monty420420@naver.com>
2023-05-20 23:04:56 +0900

    es6

--- 03_variable.js
+++ 03_variable.js
@@ -11,83 +11,90 @@
 let globalName = 'global name';
 {
 let name = 'ten';
-console.log(name);
+// console.log(name);
 name = 'hello';
-console.log(name);
+// console.log(name);
 }
-console.log(name);
-console.log(globalName);
+// console.log(name);
+// console.log(globalName);
 
 const daysInWeek = 7;
 const maxNumber = 5;
+
+//호이스팅(hoisting): 인터프리터가 변수와 함수의 메모리 공간을 선언 전에 미리 할당하는 것을 의미합니다. 
+//var 로 선언한 변수의 경우 호이스팅 시 undefined 로 변수를 초기화합니다.  
+//변수를 선언하기전에 출력을 해도 가능함 하지만 선언한 결과가아닌 undefined가 나옴
+console.log(aaa)  //출력을 먼저
+var aaa = 'da';  //선언을 나중에
+
 
 //3. Variable types
 //변수타입 설정안해도됨 
 const count = 18;
 const size = 12.1;
-console.log(`value: ${count}, type: ${typeof count}`);
-console.log(`value: ${size}, size: ${typeof size}`);
+//console.log(`value: ${count}, type: ${typeof count}`); //``백틱사용시 ${}사용해서 문자타입사이에 변수넣어줄수있음
+//console.log(`value: ${size}, size: ${typeof size}`);
 
 //number
 const infinity = 1/0;
 const negativeInfinity = -1/0;
 const nAn = 'not a number' /2
-console.log(infinity);
-console.log(negativeInfinity);
-console.log(nAn);
+// console.log(infinity);
+// console.log(negativeInfinity);
+// console.log(nAn);
 
 //string
 const char = 'c';
 const brendan ='brendan';
 const greeting = 'hello' + brendan;
-console.log(`value: ${greeting}, type: ${typeof greeting}`);
+// console.log(`value: ${greeting}, type: ${typeof greeting}`);
 const helloBob =`h1 ${brendan}!`;
-console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+// console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
 
 //boolean
 //false: 0, null, undefined, NaN,''
 //true: any other value
 const canRead = true;
 const test = 3<1; //false
-console.log(`value: ${canRead}, type: ${typeof canRead}`);
-console.log(`value: ${test}, type: ${typeof test}`);
+// console.log(`value: ${canRead}, type: ${typeof canRead}`);
+// console.log(`value: ${test}, type: ${typeof test}`);
 
 //null
 let nothing = null;
-console.log(`value: ${nothing}, type: ${typeof nothing}`);
+// console.log(`value: ${nothing}, type: ${typeof nothing}`);
 //undefined
 let x = undefined; //둘다 undefined
 //let x; //둘다 undefined
-console.log(`value: ${x}, type: ${typeof x}`);
+// console.log(`value: ${x}, type: ${typeof x}`);
 
 //symbol 
 const symbol1 = Symbol('id');
 const symbol2 = Symbol('id');
-console.log(symbol1 === symbol2);
+// console.log(symbol1 === symbol2);
 const gSymbol1 = Symbol.for('id');
 const gSymbol2 = Symbol.for('id');
-console.log(gSymbol1 === gSymbol2);
-console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
+// console.log(gSymbol1 === gSymbol2);
+// console.log(`value: ${symbol1.description}, type: ${typeof symbol1.description}`);
 /*symbol은 바로 출력하려면 .decription으로 형변환해주어야함 */
 
 //object 
 const ten = {name: 'ten', age: 27};
 ten.age = 22; //나이변경
-console.log(ten.age);
+// console.log(ten.age);
 // 5. Dynamic typing 서로 다른 값 계산시 문제가 생길수 있음
 let text = 'hello'; //문자로 선언
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = 1;  //number로 변환
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '7' + 5;
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '8' / '2';
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '3' / '1';
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '3' / '1';
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '3' / '1';
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
 text = '3' / '1';
-console.log(`value: ${text}, type: ${typeof text}`);
+// console.log(`value: ${text}, type: ${typeof text}`);
