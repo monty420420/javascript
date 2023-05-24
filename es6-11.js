3e431056bb156db8caaff76eda54dcbea9ee80ef
Muntaeyeon <monty420420@naver.com>
2023-05-24 17:55:02 +0900

    spread22

--- es6-11.js
+++ es6-11.js
@@ -47,13 +47,36 @@ const animals = ['🐶','🐱']
 {
     const dog = animals[0];
     const cat = animals[1];
-    console.log(dog);
-    console.log(cat);    
+    // console.log(dog);
+    // console.log(cat);    
 }
 
 {
   const [first, second] = animals;
   // console.log(first,second);
+}
+
+
+//spread syntax
+//기존배열을 건드리지 않고 새로운 배열에 기존배열의 내용을 사용할수있음
+{
+  const obj1 = { key: 'key1'};
+  const obj2 = { key: 'key2'};
+  const array = [obj1, obj2];
+
+  const arrayCopy = [...array];
+  // console.log(array, arrayCopy);
+  
+
+  //배열에 추가하는법 복사한 ...array에 {key: key3}넣기
+  const arrayCopy2 = [...array, { key: 'key3'}];
+  
+  //배열을 직접 건드리면 다 바뀌므로 주의
+  obj1.key = 'newKey';  // key를 newKey로 변경하면 다른배열도 다 바뀜
+  // console.log(array, arrayCopy, obj1);
+
+  //배열 합치기
+  
 }
 
 
