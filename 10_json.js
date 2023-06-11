2fd0062f1520dd993fcb36c039f3ecc9096feb05
Muntaeyeon <monty420420@naver.com>
2023-06-11 13:37:54 +0900

    json

--- 10_json.js
+++ 10_json.js
@@ -3,10 +3,10 @@
 //1. object to json
 //stringfy(obj)
 let json = JSON.stringify(true);
-console.log(json);
+// console.log(json);
 
-json = JSON.stringify(['apple', 'banana']);
-console.log(json);
+json = JSON.stringify(['apple', 'banana']); //stringify 함수 문자화
+ //console.log(json); //json규격 배열형태로 보여줌
 
 
 
@@ -22,30 +22,31 @@ const rabbit = {
 };
 
 json = JSON.stringify(rabbit);
-console.log(json);
+// console.log(json);
 
-json = JSON .stringify(rabbit, ['name']);
-console.log(json);
+json = JSON.stringify(rabbit, ['name']); //원하는 property만 문자화
+// console.log(json);
 
-json = JSON .stringify(rabbit, (key, value) => {
-    console.log(`key: ${key}, value: ${value}`);
-    return key === 'name' ? 'ten' : value;
+json = JSON.stringify(rabbit, (key, value) => {      //replacer
+    // console.log(`key: ${key}, value: ${value}`);
+    //return  value; //기본값
+    return key === 'name' ? 'ten' : value; //key의 name을 ten으로변경 아니면 value
 });
-console.log(json);
+// console.log(json);
 
 
 //2. json to object
 //parse(json)
-console.clear();
+// console.clear();
 json = JSON.stringify(rabbit);
-console.log(json);
+// console.log(json);
 const obj = JSON.parse(json, (key, value) => {
-    console.log(`key: ${key}, value: ${value}`);
+    // console.log(`key: ${key}, value: ${value}`);
     return key === 'birthDate' ? new Date(value) : value;
 });
-console.log(obj);
-rabbit.jump();
+// console.log(obj);
+// rabbit.jump();
 //obj.jump();
 
 console.log(rabbit.birthDate.getDate());
-console.log(obj.birthDate.getDate());  //error
+//console.log(obj.birthDate.getDate());  //error //string이라서 에러나옴
